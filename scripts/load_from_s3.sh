4b64217e08ab49addf6f6e23f86166d08214eed6
Buts <D124585@NAEAST.AD.JPMORGANCHASE.com>
2023-06-09 16:29:51 -0500

    deleted scripts and notebooks

--- scripts/load_from_s3.sh
+++
@@ -1,9 +0,0 @@
-#/bin/bash
-
-aws s3 sync s3://app-id-108383-dep-id-105006-uu-id-ahc5pxjsqas8/omni-q/data/labs/ data/
-
-# Has the fourier pickle files run with overlap as the objective function
-aws s3 sync s3://app-id-108383-dep-id-105006-uu-id-ahc5pxjsqas8/data/labs_fourier data_fourier/
-
-# Has the heatmap pickle files run with overlap as the objective function 
-aws s3 sync s3://app-id-108383-dep-id-105006-uu-id-ahc5pxjsqas8/data/labs_overlap data_overlap/ 
--- scripts/run_train_labs_with_fourier_init.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-parallel \
-    --linebuffer \
-    --jobs 6 \
-    """
-    python train_labs_with_fourier_init.py {1}
-    """ ::: $(seq 18 23) 
