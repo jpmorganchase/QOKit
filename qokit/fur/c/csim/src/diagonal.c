243e1b5670366b84e04366a798a78ff960bfd713
Buts <D124585@NAEAST.AD.JPMORGANCHASE.com>
2023-08-02 16:25:44 -0400

    added headded to c code

--- qokit/fur/c/csim/src/diagonal.c
+++ qokit/fur/c/csim/src/diagonal.c
@@ -1,3 +1,7 @@
+/***************************************
+* // SPDX-License-Identifier: Apache-2.0
+* // Copyright : JP Morgan Chase & Co
+****************************************/ 
 #include <stdlib.h>
 #include <math.h>
 #include <omp.h>
--- qokit/fur/c/csim/src/fur.h
+++ qokit/fur/c/csim/src/fur.h
@@ -1,3 +1,8 @@
+/***************************************
+* // SPDX-License-Identifier: Apache-2.0
+* // Copyright : JP Morgan Chase & Co
+****************************************/ 
+
 #pragma once
 #include <stddef.h>
 
