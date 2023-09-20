40f2e11ab636c9fce484222ddfcc26b380483599
Alex B <89206887+alex124585@users.noreply.github.com>
2023-09-20 16:47:01 -0400

    Update labs.py
    corrected seed

--- qokit/labs.py
+++ qokit/labs.py
@@ -521,7 +521,7 @@ def get_gate_optimized_terms_greedy(N: int, number_of_gate_zones: int = 4, seed:
 
     circuit = []
 
-    seed = seed if seed else np.random.randint(sys.maxsize)
+    seed = seed if seed else np.random.randint(np.iinfo(np.int32).max)
     print(f"seed: {seed}")
     rng = np.random.default_rng(seed)
 
