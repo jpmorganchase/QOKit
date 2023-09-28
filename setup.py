367b56fb4d7637255e95ee6919a6b0320c00fa13
Ganesan <O543910@NAEAST.AD.JPMORGANCHASE.com>
2023-09-28 11:24:35 -0400

    Test GitHub Action in Feature Branch

--- setup.py
+++ setup.py
@@ -38,56 +38,8 @@ with open("README.md", "r") as f:
     long_description = f.read()
 
 
-
-
-
-""" try:
-    from wheel.bdist_wheel import bdist_wheel as _bdist_wheel
-    class MyWheel(_bdist_wheel):
-
-        def finalize_options(self):
-            _bdist_wheel.finalize_options(self)
-            self.root_is_pure = False
-
-        def get_tag(self):
-            python, abi, plat = _bdist_wheel.get_tag(self)
-            python, abi = 'py3', 'none'
-            return python, abi, plat
-
-    class MyDistribution(Distribution):
-
-        def __init__(self, *attrs):
-            Distribution.__init__(self, *attrs)
-            self.cmdclass['bdist_wheel'] = MyWheel
-
-        def is_pure(self):
-            return False
-
-        def has_ext_modules(self):
-            return True
-
-except ImportError:
-    class MyDistribution(Distribution):
-        def is_pure(self):
-            return False
-
-        def has_ext_modules(self):
-            return True """
-
-try:
-    from wheel.bdist_wheel import bdist_wheel as _bdist_wheel
-    class bdist_wheel(_bdist_wheel):
-        def finalize_options(self):
-            _bdist_wheel.finalize_options(self)
-            self.root_is_pure = False
-except ImportError:
-    bdist_wheel = None
-
-
-
 setup(
     ext_modules=extensions,
-    cmdclass={"build_ext": SimulatorBuild, "bdist_wheel": bdist_wheel},
+#    cmdclass={"build_ext": SimulatorBuild, "bdist_wheel": bdist_wheel},
     packages=find_packages(),
-#    distclass=MyDistribution
 ) 
