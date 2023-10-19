b2b0e343da252b45af9970358c10de5cc4059251
Ganesan <O543910@NAEAST.AD.JPMORGANCHASE.com>
2023-10-19 11:48:12 -0400

    Merged with main branch to bring it upto date with feature branch

--- setup.py
+++ setup.py
@@ -45,4 +45,8 @@ with open("README.md", "r") as f:
     long_description = f.read()
 
 
-setup(ext_modules=extensions, cmdclass={"build_ext": SimulatorBuild}, packages=find_namespace_packages(), include_package_data=True)
+setup(
+    ext_modules=extensions,
+    cmdclass={"build_ext": SimulatorBuild} if sys.platform == "win32" else {},
+    packages=find_packages(),
+)
