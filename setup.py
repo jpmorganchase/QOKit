df2db8e86d1941e998d910c5fbbf758dd2473060
Ruslan Shaydulin <rsln-s@users.noreply.github.com>
2023-10-23 09:13:49 -0400

    Remove unused imports from setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 # // SPDX-License-Identifier: Apache-2.0
 # // Copyright : JP Morgan Chase & Co
 ###############################################################################
-from setuptools import setup, find_packages, Extension, find_namespace_packages, glob
+from setuptools import setup, Extension
 from setuptools.command.build_ext import build_ext
 import subprocess
 import os
