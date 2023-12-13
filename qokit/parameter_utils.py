flipped gamma sign in get_fixed_gamma_beta and fixed the test cases

qokit/parameter_utils.py
@@ -210,6 +210,7 @@ def get_sk_gamma_beta(p, parameterization: QAOAParameterization | str = "gamma b
     if parameterization == QAOAParameterization.THETA:
         return np.concatenate((4 * gamma, beta), axis=0)
     elif parameterization == QAOAParameterization.GAMMA_BETA:
+        # return -1 * (4 * gamma), beta
         return 4 * gamma, beta
 
 
@@ -255,9 +256,9 @@ def get_fixed_gamma_beta(d, p, return_AR=False):
         raise ValueError(f"Failed to retrieve fixed angles for d={d}, p={p}")
     row = row.squeeze()
     if return_AR:
-        return row["gamma"], row["beta"], row["AR"]
+        return -1 * np.asarray(row["gamma"]), row["beta"], row["AR"]
     else:
-        return row["gamma"], row["beta"]
+        return -1 * np.asarray(row["gamma"]), row["beta"]
 
 
 def get_best_known_parameters_for_LABS_wrt_overlap(N: int) -> pd.DataFrame:

tests/test_maxcut.py
@@ -50,10 +50,8 @@ def test_maxcut_qaoa_obj_fixed_angles():
 
         obj = partial(maxcut_obj, w=get_adjacency_matrix(G))
         optimal_cut, x = brute_force(obj, N, function_takes="bits")
-
         for p in range(1, max_p + 1):
             gamma, beta, AR = get_fixed_gamma_beta(d, p, return_AR=True)
-            gamma = -1 * np.asarray(gamma)
             for simulator in ["auto", "qiskit"]:
                 f = get_qaoa_maxcut_objective(N, p, G=G, parameterization="gamma beta", simulator=simulator)
                 assert abs(f(gamma, beta) / optimal_cut) > AR
@@ -67,17 +65,18 @@ def test_maxcut_weighted_qaoa_obj():
         lambda row: nx.node_link_graph(row["G_json"]),
         axis=1,
     )
+    # changing the sign of gamma got from file
+    df["gamma"] = df["gamma"].apply(lambda x: [-y for y in x])
 
     for _, row in df.iterrows():
         for simulator in ["auto", "qiskit"]:
             f = get_qaoa_maxcut_objective(row["G"].number_of_nodes(), row["p"], G=row["G"], parameterization="gamma beta", simulator=simulator)
-            assert np.isclose(abs(f(-1 * np.asarray(row["gamma"]), row["beta"])), row["Expected cut of QAOA"])
+            assert np.isclose(abs(f(row["gamma"], row["beta"])), row["Expected cut of QAOA"])
 
         # Qiskit non-parameterized circuit must be tested separately
         precomputed_cuts = precompute_energies(maxcut_obj, row["G"].number_of_nodes(), w=get_adjacency_matrix(row["G"]))
-        qc = get_qaoa_circuit(row["G"], row["beta"], -1 * np.asarray(row["gamma"]))
-        qc_param = get_parameterized_qaoa_circuit(row["G"], row["p"]).bind_parameters(np.hstack([row["beta"], -1 * np.asarray(row["gamma"])]))
-
+        qc = get_qaoa_circuit(row["G"], row["beta"], row["gamma"])
+        qc_param = get_parameterized_qaoa_circuit(row["G"], row["p"]).bind_parameters(np.hstack([row["beta"], row["gamma"]]))
         sv = np.asarray(qiskit_backend.run(qc).result().get_statevector())
         sv_param = np.asarray(qiskit_backend.run(qc_param).result().get_statevector())
 

tests/test_qaoa_objective_maxcut.py
@@ -30,6 +30,6 @@ def test_validate_energy_for_terms_with_simulators_are_same():
     g = get_qaoa_maxcut_objective(G.number_of_nodes(), 1, G=G, parameterization="gamma beta", simulator="qiskit")
     p = 1
     gamma, beta = get_fixed_gamma_beta(3, p)
-    auto = f(-1 * np.asarray(gamma), beta)
-    qiskit = g(-1 * np.asarray(gamma), beta)
+    auto = f(gamma, beta)
+    qiskit = g(gamma, beta)
     assert np.isclose(auto, qiskit)