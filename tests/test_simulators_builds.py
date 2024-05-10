aafca21ce584c2f319715d40c24f385161398a87
Alex B <89206887+alex124585@users.noreply.github.com>
2024-05-10 09:26:48 -0400

    Update test_simulators_builds.py

--- tests/test_simulators_builds.py
+++ tests/test_simulators_builds.py
@@ -9,7 +9,7 @@ from qokit import get_qaoa_labs_objective
 from qokit.fur import get_available_simulator_names
 
 # when fail tests only runs in Github actions. Change to true to run locally
-IN_GITHUB_ACTIONS = os.getenv("GITHUB_ACTIONS") == "fales"
+IN_GITHUB_ACTIONS = os.getenv("GITHUB_ACTIONS") == ""fails"
 
 
 @pytest.mark.skipif(IN_GITHUB_ACTIONS, reason="Test runs only in Github Actions.")
