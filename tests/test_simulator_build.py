e0ebef82d90c341c05c3726ceb70d41d11ccb0e7
Shaydulin, Ruslan <ruslan.shaydulin@jpmchase.com>
2024-08-01 21:35:37 +0000

    now build tests are run correctly

--- tests/test_simulator_build.py
+++ tests/test_simulator_build.py
@@ -13,14 +13,14 @@ PYTHON_ONLY = os.environ.get("QOKIT_PYTHON_ONLY") == "true"
 
 
 @pytest.mark.skipif(PYTHON_ONLY, reason="Fast c/c++ simulator is not installed")
-def test_simulator_c_bild():
+def test_simulator_c_build():
     assert "c" in get_available_simulator_names("x")
     assert "c" in get_available_simulator_names("xyring")
     assert "c" in get_available_simulator_names("xycomplete")
 
 
 @pytest.mark.skipif(not PYTHON_ONLY, reason="Fast c/c++ simulator should be installed")
-def test_simulator_c_bild():
+def test_simulator_lack_of_c_build():
     assert "c" not in get_available_simulator_names("x")
     assert "c" not in get_available_simulator_names("xyring")
     assert "c" not in get_available_simulator_names("xycomplete")
@@ -32,11 +32,9 @@ def test_simulator_python_build():
     assert "python" in get_available_simulator_names("xycomplete")
 
 
-@pytest.mark.skipif(not IN_GITHUB_ACTIONS, reason="Test runs only in Github Actions.")
 @pytest.mark.skipif(PYTHON_ONLY, reason="Fast c/c++ simulator is not installed")
 @pytest.mark.timeout(10)
-def test_simulator_timing_test():
+def test_simulator_timing():
     theta = np.random.uniform(0, 1, 280)
     f = get_qaoa_labs_objective(20, 140)
     f(theta)
-    pass
