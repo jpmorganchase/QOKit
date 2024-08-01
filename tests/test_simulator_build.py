18d075776dc42f06ae0322677a1a3da1c50acb1e
Shaydulin, Ruslan <ruslan.shaydulin@jpmchase.com>
2024-08-01 21:08:45 +0000

    no need to skip the tests

--- tests/test_simulator_build.py
+++ tests/test_simulator_build.py
@@ -12,7 +12,6 @@ IN_GITHUB_ACTIONS = os.getenv("GITHUB_ACTIONS") == "true"
 PYTHON_ONLY = os.environ.get("QOKIT_PYTHON_ONLY") == "true"
 
 
-@pytest.mark.skipif(not IN_GITHUB_ACTIONS, reason="Test runs only in Github Actions.")
 @pytest.mark.skipif(PYTHON_ONLY, reason="Fast c/c++ simulator is not installed")
 def test_simulator_c_bild():
     assert "c" in get_available_simulator_names("x")
@@ -20,7 +19,6 @@ def test_simulator_c_bild():
     assert "c" in get_available_simulator_names("xycomplete")
 
 
-@pytest.mark.skipif(not IN_GITHUB_ACTIONS, reason="Test runs only in Github Actions.")
 @pytest.mark.skipif(not PYTHON_ONLY, reason="Fast c/c++ simulator should be installed")
 def test_simulator_c_bild():
     assert "c" not in get_available_simulator_names("x")
@@ -28,7 +26,6 @@ def test_simulator_c_bild():
     assert "c" not in get_available_simulator_names("xycomplete")
 
 
-@pytest.mark.skipif(not IN_GITHUB_ACTIONS, reason="Test runs only in Github Actions.")
 def test_simulator_python_build():
     assert "python" in get_available_simulator_names("x")
     assert "python" in get_available_simulator_names("xyring")
