ed4d66481ea9e242727b5cfe6594ad505da73ab1
Shaydulin, Ruslan <ruslan.shaydulin@jpmchase.com>
2024-08-01 16:41:53 -0400

    fix flag

--- tests/test_simulators_builds.py
+++ tests/test_simulators_builds.py
@@ -10,11 +10,11 @@ from qokit.fur import get_available_simulator_names
 
 # for GITHUB_ACTIONS = false tests are runing in Github actions only. Change to true to run locally
 
-IN_GITHUB_ACTIONS = os.getenv("GITHUB_ACTIONS") == "false"
+IN_GITHUB_ACTIONS = os.getenv("GITHUB_ACTIONS") == "true"
 python_only = os.environ.get("QOKIT_PYTHON_ONLY")
 
 
-@pytest.mark.skipif(IN_GITHUB_ACTIONS, reason="Test runs only in Github Actions.")
+@pytest.mark.skipif(not IN_GITHUB_ACTIONS, reason="Test runs only in Github Actions.")
 @pytest.mark.skipif(python_only, reason="Fast c/c++ simulator is not installed")
 def test_simulator_c_bild():
     assert "c" in get_available_simulator_names("x")
@@ -22,14 +22,14 @@ def test_simulator_c_bild():
     assert "c" in get_available_simulator_names("xycomplete")
 
 
-@pytest.mark.skipif(IN_GITHUB_ACTIONS, reason="Test runs only in Github Actions.")
+@pytest.mark.skipif(not IN_GITHUB_ACTIONS, reason="Test runs only in Github Actions.")
 def test_simulator_python_build():
     assert "python" in get_available_simulator_names("x")
     assert "python" in get_available_simulator_names("xyring")
     assert "python" in get_available_simulator_names("xycomplete")
 
 
-@pytest.mark.skipif(IN_GITHUB_ACTIONS, reason="Test runs only in Github Actions.")
+@pytest.mark.skipif(not IN_GITHUB_ACTIONS, reason="Test runs only in Github Actions.")
 @pytest.mark.skipif(python_only, reason="Fast c/c++ simulator is not installed")
 @pytest.mark.timeout(10)
 def test_simulator_timing_test():
