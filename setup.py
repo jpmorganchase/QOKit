96ec72cfb45133c9e42899237fbd4ab44a9cb353
Shaydulin, Ruslan <ruslan.shaydulin@jpmchase.com>
2024-08-05 20:15:49 -0400

    unclear why cmdclass was added only for win32

--- setup.py
+++ setup.py
@@ -35,4 +35,4 @@ class SimulatorBuild(build_ext):
 with open("README.md", "r") as f:
     long_description = f.read()
 
-setup(ext_modules=extensions, cmdclass={"build_ext": SimulatorBuild} if sys.platform == "win32" else {}),
+setup(ext_modules=extensions, cmdclass={"build_ext": SimulatorBuild})
