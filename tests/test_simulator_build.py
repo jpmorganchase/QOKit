25e5b726cb383021fc695bb01617a28ed6a5675c
Ganesan <O543910@NAEAST.AD.JPMORGANCHASE.com>
2024-08-08 09:25:14 -0400

    add C benchmark

--- tests/test_simulator_build.py
+++ tests/test_simulator_build.py
@@ -8,25 +8,30 @@ import numpy as np
 from qokit import get_qaoa_labs_objective
 from qokit.fur import get_available_simulator_names
 
-os.environ.set("QOKIT_PYTHON_ONLY", True)
-PYTHON_ONLY = os.environ.get("QOKIT_PYTHON_ONLY")
 
+PYTHON_ONLY = os.environ.get("QOKIT_PYTHON_ONLY")  # By default it is False
 
-# @pytest.mark.skipif(PYTHON_ONLY, reason="Fast c/c++ simulator is not installed")
-# def test_simulator_c_build():
-#     assert "c" in get_available_simulator_names("x")
-#     assert "c" in get_available_simulator_names("xyring")
-#     assert "c" in get_available_simulator_names("xycomplete")
-
-
-@pytest.mark.skipif(not PYTHON_ONLY, reason="Fast c/c++ simulator should be installed")
-def test_simulator_lack_of_c_build():
-    assert "c" not in get_available_simulator_names("x")
-    assert "c" not in get_available_simulator_names("xyring")
-    assert "c" not in get_available_simulator_names("xycomplete")
+PYTHON_ONLY = False
 
 
 @pytest.mark.skipif(PYTHON_ONLY, reason="Fast c/c++ simulator is not installed")
+def test_simulator_c_build():
+    assert "c" in get_available_simulator_names("x")
+    assert "c" in get_available_simulator_names("xyring")
+    assert "c" in get_available_simulator_names("xycomplete")
+
+
+# @pytest.mark.skipif(PYTHON_ONLY, reason="Fast c/c++ simulator should be installed")
+# def test_simulator_lack_of_c_build():
+#     assert "c" not in get_available_simulator_names("x")
+#     assert "c" not in get_available_simulator_names("xyring")
+#     assert "c" not in get_available_simulator_names("xycomplete")
+
+
+PYTHON_ONLY = True
+
+
+@pytest.mark.skipif(not PYTHON_ONLY, reason="Fast c/c++ simulator is not installed")
 def test_simulator_python_build():
     assert "python" in get_available_simulator_names("x")
     assert "python" in get_available_simulator_names("xyring")
