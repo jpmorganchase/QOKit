3d3a5141034a4f101f6f62d01a8311858023fca8
Alex B <89206887+alex124585@users.noreply.github.com>
2024-10-09 13:32:44 -0400

    Update test_simulator_build.py
    Add simulator build test

--- tests/test_simulator_build.py
+++ tests/test_simulator_build.py
@@ -1,1 +1,25 @@
+###############################################################################
+# // SPDX-License-Identifier: Apache-2.0
+# // Copyright : JP Morgan Chase & Co
+###############################################################################
+import pytest
+import os
+import numpy as np
+from qokit import get_qaoa_labs_objective
+from qokit.fur import get_available_simulator_names
 
+# Set up QOKIT_PYTHON_ONLY in your local enviroment for Python only
+PYTHON_ONLY = os.environ.get("QOKIT_PYTHON_ONLY")
+
+
+@pytest.mark.skipif(not PYTHON_ONLY, reason="Fast c/c++ simulator should be installed")
+def test_simulator_lack_of_c_build():
+    assert "c" not in get_available_simulator_names("x")
+    assert "c" not in get_available_simulator_names("xyring")
+    assert "c" not in get_available_simulator_names("xycomplete")
+
+
+def test_simulator_python_build():
+    assert "python" in get_available_simulator_names("x")
+    assert "python" in get_available_simulator_names("xyring")
+    assert "python" in get_available_simulator_names("xycomplete")
