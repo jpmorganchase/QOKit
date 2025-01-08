fb27cce203455f03ab317fb8f80acf120f0c20f9
bgupt <brajeshgupt@gmail.com>
2025-01-08 20:59:02 +0000

    Adding test to check C Simulator build.

---
+++ tests/test_simulator_build.py
@@ -0,0 +1,25 @@
+###############################################################################
+# // SPDX-License-Identifier: Apache-2.0
+# // Copyright : JP Morgan Chase & Co
+###############################################################################
+import pytest
+import os
+import numpy as np
+from qokit import get_qaoa_labs_objective
+from qokit.fur import get_available_simulator_names
+
+# Set up QOKIT_PYTHON_ONLY in your local enviroment for Python only
+PYTHON_ONLY = False if os.environ.get("QOKIT_PYTHON_ONLY") is None else os.environ.get("QOKIT_PYTHON_ONLY")
+
+
+@pytest.mark.skipif(PYTHON_ONLY, reason="Fast c/c++ simulator should be installed")
+def test_simulator_lack_of_c_build():
+    print("Condition: ", PYTHON_ONLY)
+    assert "c" in get_available_simulator_names("x")
+    assert "c" in get_available_simulator_names("xyring")
+    assert "c" in get_available_simulator_names("xycomplete")
+
+def test_simulator_python_build():
+    assert "python" in get_available_simulator_names("x")
+    assert "python" in get_available_simulator_names("xyring")
+    assert "python" in get_available_simulator_names("xycomplete")
