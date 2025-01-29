5dc0b9ba0be94f6589a782235f073bc77bb904f6
bgupt <brajeshgupt@gmail.com>
2025-01-29 19:14:14 +0000

    Changing black version.

--- qokit/yahoo.py
+++ qokit/yahoo.py
@@ -1,6 +1,7 @@
 """ Yahoo data provider. 
 
-    Based on Qiskit YahooDataProvider: https://qiskit-community.github.io/qiskit-finance/_modules/qiskit_finance/data_providers/yahoo_data_provider.html#YahooDataProvider
+    Based on Qiskit YahooDataProvider: https://qiskit-community.github.io/qiskit-finance/_modules/
+        qiskit_finance/data_providers/yahoo_data_provider.html#YahooDataProvider
 
 """
 
