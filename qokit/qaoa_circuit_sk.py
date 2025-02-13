Removed unused imports, fixed sign for SK model.

qokit/qaoa_circuit_sk.py
@@ -4,7 +4,6 @@
 ###############################################################################
 # QAOA circuit for S_k
 
-import networkx as nx
 import numpy as np
 from qiskit import QuantumCircuit, QuantumRegister, ClassicalRegister
 from qiskit.circuit import ParameterVector
@@ -36,7 +35,7 @@ def get_qaoa_circuit(J: np.ndarray, gammas: Sequence, betas: Sequence, save_stat
     Parameters
     ----------
     J : numpy.ndarray
-        J_ij for the SK model.
+        Matrix representing couplings in the SK model.
     beta : list-like
         QAOA parameter beta
     gamma : list-like

qokit/qaoa_objective_sk.py
@@ -2,13 +2,11 @@
 # // SPDX-License-Identifier: Apache-2.0
 # // Copyright : JP Morgan Chase & Co
 ###############################################################################
-from __future__ import annotations
 import numpy as np
-import networkx as nx
 import warnings
 
 from .utils import precompute_energies
-from .sk import sk_obj, get_adjacency_matrix, get_sk_terms
+from .sk import sk_obj, get_sk_terms
 
 from .qaoa_circuit_sk import get_parameterized_qaoa_circuit
 from .qaoa_objective import get_qaoa_objective
@@ -33,7 +31,7 @@ def get_qaoa_sk_objective(
     p : int
         Number of QAOA layers (number of parameters will be 2*p)
     J : numpy.ndarray
-        J_ij for SK model.
+        Couplinf matrix for the SK model.
     precomputed_cuts : np.array
         precomputed cuts to compute the QAOA expectation, for maximization problem
         send the precomputed cuts/energies as negative
@@ -55,7 +53,7 @@ def get_qaoa_sk_objective(
         Function returning the negative of expected value of QAOA with parameters theta
     """
     terms = None
-    optimization_type = "max"
+    optimization_type = "min"
 
     if precomputed_cuts is not None and J is not None:
         warnings.warn("If precomputed_cuts is passed, J is ignored")

qokit/sk.py
@@ -7,20 +7,20 @@ Helper functions for the S_k problem
 """
 from qokit.fur.qaoa_simulator_base import TermsType
 import numpy as np
-import networkx as nx
+from itertools import combinations
 
 
 def sk_obj(x: np.ndarray, J: np.ndarray) -> float:
-    """Compute the value of a cut.
+    """Compute the value of objective function for SK model.
     Args:
         x (numpy.ndarray): binary string as numpy array.
-        w (numpy.ndarray): adjacency matrix returned by get_adjacency_matrix
+        J (numpy.ndarray): Matrix specifying the coupling in the SK model.
     Returns:
-        float: value of the cut.
+        float: value of the objective function.
     """
     n = len(x)
     X = np.outer(2 * x - 1, 2 * x - 1)
-    return -np.sum(J * X) / np.sqrt(n)  # type: ignore
+    return np.sum(J * X) / np.sqrt(n)  # type: ignore
 
 
 def get_sk_terms(J: np.ndarray) -> TermsType:
@@ -31,34 +31,11 @@ def get_sk_terms(J: np.ndarray) -> TermsType:
         S = 1/\sqrt(N) \sum_{(i,j)\\in G} J_ij * (s_i*s_j)/2
 
     Args:
-        G: MaxCut problem graph
+        J (numpy.ndarray): Matrix specifying the coupling in the SK model.
     Returns:
         terms to be used in the simulation
     """
     N = J.shape[0]
-    G = nx.complete_graph(N)
-    for edge in G.edges:
-        G.edges[edge[0], edge[1]]["weight"] = J[edge[0], edge[1]]
 
-    terms = [(-2 * float(G[u][v]["weight"]) / np.sqrt(N), (int(u), int(v))) for u, v, *_ in G.edges()]
+    terms = [((2 * J[i, j]) / np.sqrt(N), (int(i), int(j))) for i, j in combinations(range(N), 2)]
     return terms
-
-
-def get_adjacency_matrix(G: nx.Graph) -> np.ndarray:
-    """Get adjacency matrix to be used in sk_obj
-    Args:
-        G (nx.Graph) : graph
-    Returns:
-        w (numpy.ndarray): adjacency matrix
-    """
-    n = G.number_of_nodes()
-    w = np.zeros([n, n])
-
-    for e in G.edges():
-        if nx.is_weighted(G):
-            w[e[0], e[1]] = G[e[0]][e[1]]["weight"]
-            w[e[1], e[0]] = G[e[0]][e[1]]["weight"]
-        else:
-            w[e[0], e[1]] = 1
-            w[e[1], e[0]] = 1
-    return w

tests/test_qaoa_objective_sk.py
@@ -3,27 +3,17 @@
 # // Copyright : JP Morgan Chase & Co
 ###############################################################################
 import numpy as np
-import pandas as pd
-import networkx as nx
+import pytest
 from pathlib import Path
 from functools import partial
 from qiskit_aer import AerSimulator
-import pytest
 from itertools import combinations
-
 from qokit.sk import sk_obj, get_sk_terms
-
-from qokit.maxcut import maxcut_obj, get_adjacency_matrix, get_maxcut_terms
-
+from qokit.maxcut import maxcut_obj
 from qokit.qaoa_objective_sk import get_qaoa_sk_objective
-from qokit.qaoa_objective_maxcut import get_qaoa_maxcut_objective
-
-from qokit.qaoa_circuit_sk import get_qaoa_circuit, get_parameterized_qaoa_circuit
 from qokit.utils import brute_force, precompute_energies
-from qokit.parameter_utils import get_sk_gamma_beta, get_fixed_gamma_beta
-import qokit
+from qokit.parameter_utils import get_sk_gamma_beta
 from qokit.fur import get_available_simulators, get_available_simulator_names
-
 from qokit.qaoa_objective import get_qaoa_objective
 
 test_sk_folder = Path(__file__).parent
@@ -46,7 +36,7 @@ def test_sk_obj(n=5):
         for i in range(n):
             for j in range(i, n):
                 obj += J[i, j] * (2 * x[i] - 1) * (2 * x[j] - 1)
-        return -2 * obj / np.sqrt(n)
+        return 2 * obj / np.sqrt(n)
 
     x = np.random.choice([0, 1], n)
     assert np.isclose(sk_obj(x, J), sk_obj_simple(x, J))
@@ -71,11 +61,11 @@ def test_energy_pre_optimized():
     gamma = np.array([0.2268, 0.4162, 0.4332, 0.4608, 0.4818, 0.5179, 0.5717, 0.6393])
     beta = np.array([0.6151, 0.4906, 0.4244, 0.3780, 0.3224, 0.2606, 0.1884, 0.1030])
 
-    # Precomputed expected energy with the above fixed parameters: [3.838980017541577, 3.8389800175415765]
-    expected_energy = [3.838980017541577, 3.8389800175415765]
+    # Precomputed expected energy with the above fixed parameters: [4.207818619693583, 4.207818619693583]
+    expected_energy = [4.207818619693583, 4.207818619693583]
 
     qaoa_objectives = [
-        -get_qaoa_sk_objective(N, p, J=J, parameterization="gamma beta", simulator=simulator, objective="expectation")(gamma, beta)
+        get_qaoa_sk_objective(N, p, J=J, parameterization="gamma beta", simulator=simulator, objective="expectation")(gamma, beta)
         for simulator in simulators_to_run_names_no_qiskit
     ]
     print(qaoa_objectives)
@@ -98,7 +88,7 @@ def test_sk_qaoa_convergence_with_p():
         gamma, beta = get_sk_gamma_beta(p)
         for objective in ["expectation"]:
             qaoa_objectives = [
-                -get_qaoa_sk_objective(
+                get_qaoa_sk_objective(
                     N, p, J=J, precomputed_cuts=precomputed_energies, parameterization="gamma beta", simulator=simulator, objective=objective
                 )(gamma / np.sqrt(N), beta)
                 for simulator in simulators_to_run_names_no_qiskit
@@ -115,7 +105,7 @@ def test_sk_withJ_and_maxcut_with_terms():
     J = (J + J.T) / 2
     np.fill_diagonal(J, 0)
 
-    terms = [(-2 * J[spin_pair[0], spin_pair[1]] / np.sqrt(N), spin_pair) for spin_pair in combinations(range(N), r=2)]
+    terms = [(2 * J[spin_pair[0], spin_pair[1]] / np.sqrt(N), spin_pair) for spin_pair in combinations(range(N), r=2)]
 
     for p in range(1, 18):
         gamma, beta = get_sk_gamma_beta(p)
@@ -124,7 +114,7 @@ def test_sk_withJ_and_maxcut_with_terms():
             for simulator in simulators_to_run_names_no_qiskit
         ]
         f2 = [
-            -get_qaoa_sk_objective(N, p, J=J, parameterization="gamma beta", simulator=simulator, objective="expectation")(gamma, beta)
+            get_qaoa_sk_objective(N, p, J=J, parameterization="gamma beta", simulator=simulator, objective="expectation")(gamma, beta)
             for simulator in simulators_to_run_names_no_qiskit
         ]
 
@@ -191,7 +181,7 @@ def test_sk_maxcut_bruteforce(simulator):
     obj_maxcut = partial(maxcut_obj, w=J)
     optimal_maxcut, x_maxcut = brute_force(obj_maxcut, num_variables=N, function_takes="bits")
     obj_sk = partial(sk_obj, J=J)
-    optimal_sk, x_sk = brute_force(obj_sk, num_variables=N, function_takes="bits")
+    optimal_sk, x_sk = brute_force(obj_sk, num_variables=N, minimize=True, function_takes="bits")
     x_maxcut_comp = 1 - x_maxcut
 
     assert np.allclose(x_maxcut, x_sk) or np.allclose(x_maxcut_comp, x_sk)