99a5c728261c7d9b75075b45fbda180c4c7f047b
“N772955” <zichang.he@jpmchase.com>
2025-04-30 20:19:11 +0000

    add ws and xz mixer

--- qokit/fur/c/gates.py
+++ qokit/fur/c/gates.py
@@ -24,6 +24,21 @@ def furx(sv: ComplexArray | np.ndarray, theta: float, q: int) -> ComplexArray:
     csim.furx(sv.real, sv.imag, 0.5 * theta, q)
     return sv
 
+def furxz(sv: ComplexArray | np.ndarray, theta: float, init_rots: np.ndarray, q: int) -> ComplexArray:
+    """
+    apply to a statevector a single-qubit Pauli-XZ rotation defined by
+    Rx(theta) = exp(-i*theta*X/2)
+    where X is the Pauli-X operator.
+    The operation will be in-place if the input is a ComplexArray, otherwise (NumPy array)
+    the input will be copied to a new ComplexArray.
+    @param sv statevector on which the rotation is applied (ComplexArray or numpy.ndarray)
+    @param theta rotation angle
+    @param q index of qubit on which the rotation is applied
+    @return a ComplexArray containing the statevector after the operation
+    """
+    sv = get_complex_array(sv)
+    csim.furxz(sv.real, sv.imag, 0.5 * theta, init_rots, q)
+    return sv
 
 def furxy(sv: ComplexArray | np.ndarray, theta: float, q1: int, q2: int) -> ComplexArray:
     """
