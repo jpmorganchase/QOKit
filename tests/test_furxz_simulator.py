c75525f563144f1a0f45119c8f6e777966f558da
“N772955” <zichang.he@jpmchase.com>
2025-05-01 18:23:14 +0000

    add qiskit circuits

--- tests/test_furxz_simulator.py
+++ tests/test_furxz_simulator.py
@@ -3,6 +3,8 @@ from qokit.maxcut import get_maxcut_terms
 from qokit.parameter_utils import get_fixed_gamma_beta
 import qokit
 import numpy as np
+from qokit.qaoa_circuit_maxcut import get_ws_qaoa_circuit
+from qiskit_aer import AerSimulator
 
 def test_furxz_backends():
 
@@ -26,3 +28,28 @@ def test_furxz_backends():
     python_energy = sim.get_expectation(_result)
     
     assert np.isclose(c_energy, python_energy)
+    
+def test_qiskit_qokit():
+    ##### qiskit circuit 
+    
+    N = 10
+    d = 3
+    p = 1
+    seed = 1
+    G = nx.random_regular_graph(d,N,seed=seed)
+    terms = get_maxcut_terms(G)
+    gamma, beta = get_fixed_gamma_beta(d=d,p=p)
+    ini_rots = np.random.rand(N)
+    
+    simclass = qokit.fur.choose_simulator_xz(name='c')
+    sim = simclass(N, terms=terms)
+    _result = sim.simulate_ws_qaoa(list(np.asarray(gamma)), list(np.asarray(beta)), ini_rots)
+    qaoa_prob = sim.get_probabilities(_result)
+    
+    qc = get_ws_qaoa_circuit(G, gamma, beta, ini_rots)
+    backend = AerSimulator(method="statevector")
+    sv = np.asarray(backend.run(qc).result().get_statevector())
+    qiskit_prob = np.abs(sv) ** 2
+    
+    assert np.allclose(qaoa_prob, qiskit_prob)
+    
