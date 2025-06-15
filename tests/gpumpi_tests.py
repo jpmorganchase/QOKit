c95226469e6115d7b739aabba348aadef8a3a684
Zichang He <quantumcheese@x3006c0s19b0n0.hsn.cm.polaris.alcf.anl.gov>
2025-06-15 01:15:35 +0000

    polish the test functions

--- tests/gpumpi_tests.py
+++ tests/gpumpi_tests.py
@@ -2,61 +2,73 @@ import pandas as pd
 import numpy as np
 import networkx as nx
 from functools import partial
-from qokit.maxcut import maxcut_obj, get_adjacency_matrix, get_maxcut_terms
+from qokit.maxcut import maxcut_obj, get_adjacency_matrix
 from qokit.qaoa_objective_labs import get_qaoa_labs_objective
 from qokit.qaoa_objective_maxcut import get_qaoa_maxcut_objective
 from qokit.sk import sk_obj, get_random_J
 from qokit.qaoa_objective_sk import get_qaoa_sk_objective
-from qokit.utils import brute_force, precompute_energies
-from qokit.labs import get_terms
-import subprocess
-from qokit.fur.mpi_nbcuda.qaoa_simulator import mpi_available
-from qokit.fur import get_available_simulator_names, choose_simulator
+from qokit.utils import precompute_energies
+from pathlib import Path
 
+SCRIPT_DIR = Path(__file__).resolve().parent
 
-def gpumpi_test(N=16, p=4, seed=1):
-    # LABS
-    df = pd.read_json("./qokit/assets/QAOA_with_fixed_parameters_p_opt.json", orient="index")
+def gpumpi_labs_test(N=10, p=4):
+    df = pd.read_json(f"{SCRIPT_DIR.parent}/qokit/assets/QAOA_with_fixed_parameters_p_opt.json", orient="index")
     row = df[(df["N"] == N) & (df["p"] == p)]
     beta = row["beta"].values[0]
     gamma = row["gamma"].values[0]
     overlap_trans_labs = float(row["overlap transferred"].values[0])
-    f_gpumpi_labs = get_qaoa_labs_objective(N, p, simulator="gpumpi", objective="overlap", parameterization="gamma beta")
-    overlap_trans_computed_labs = 1 - f_gpumpi_labs(gamma, beta)
+    for objective in ["overlap", "expectation"]:
+        f_gpumpi_labs = get_qaoa_labs_objective(N, p, simulator="gpumpi", objective=objective, parameterization="gamma beta")
+        f_c_labs = get_qaoa_labs_objective(N, p, simulator="c", objective=objective, parameterization="gamma beta")
+        if objective == "overlap":
+            overlap_trans_gpumpi_computed_labs = 1 - f_gpumpi_labs(gamma, beta)
+            overlap_trans_c_computed_labs = 1 - f_c_labs(gamma, beta)
+            assert np.isclose(overlap_trans_gpumpi_computed_labs, overlap_trans_labs)
+            assert np.isclose(overlap_trans_gpumpi_computed_labs, overlap_trans_c_computed_labs)
+        elif objective == "expectation":
+            assert np.isclose(f_gpumpi_labs(gamma, beta), f_c_labs(gamma, beta))
 
-    print(np.isclose(overlap_trans_computed_labs, overlap_trans_labs))
-
-    # Maxcut
+def gpumpi_maxcut_test(N=16, p=4, seed=1):
     d = 3
     G = nx.random_regular_graph(d, N, seed=seed)
+    gamma, beta = np.random.rand(p), np.random.rand(p)
     obj_maxcut = partial(maxcut_obj, w=get_adjacency_matrix(G))
     precomputed_energies = precompute_energies(obj_maxcut, N)
+    for objective in ["overlap", "expectation"]:
+        o1_c_maxcut = get_qaoa_maxcut_objective(
+            N, p, precomputed_cuts=precomputed_energies, simulator="c", parameterization="gamma beta", objective=objective
+        )(gamma, beta)
+        o1_gpumpi_maxcut = get_qaoa_maxcut_objective(
+            N, p, precomputed_cuts=precomputed_energies, simulator="gpumpi", parameterization="gamma beta", objective=objective
+        )(gamma, beta)
+        o2_gpumpi_maxcut = get_qaoa_maxcut_objective(
+            N, p, G=G, simulator="gpumpi", parameterization="gamma beta", objective=objective
+        )(gamma, beta)
+        assert np.isclose(o1_c_maxcut, o1_gpumpi_maxcut) 
+        assert np.isclose(o1_c_maxcut, o2_gpumpi_maxcut)
 
-    o1_c_maxcut = get_qaoa_maxcut_objective(N, p, precomputed_cuts=precomputed_energies, simulator="c", parameterization="gamma beta", objective="overlap")(
-        gamma, beta
-    )
-    o1_gpumpi_maxcut = get_qaoa_maxcut_objective(
-        N, p, precomputed_cuts=precomputed_energies, simulator="gpumpi", parameterization="gamma beta", objective="overlap"
-    )(gamma, beta)
-    o2_gpumpi_maxcut = get_qaoa_maxcut_objective(N, p, G=G, simulator="gpumpi", parameterization="gamma beta", objective="overlap")(gamma, beta)
-    print(np.all([o1_c_maxcut == np.real(o1_gpumpi_maxcut), o1_c_maxcut == np.real(o2_gpumpi_maxcut)]))
+def gpumpi_sk_test(N=10, p=3, seed=42):
 
-    # SK
-    J = get_random_J(N=N)
+    J = get_random_J(N=N, seed=seed)
+    gamma, beta = np.random.rand(p), np.random.rand(p)
     obj_sk = partial(sk_obj, J=J)
     precomputed_energies = precompute_energies(obj_sk, N)
-
-    o1_c_sk = get_qaoa_sk_objective(N, p, J=J, precomputed_energies=precomputed_energies, simulator="c", parameterization="gamma beta", objective="overlap")(
-        gamma, beta
-    )
-    o1_gpumpi_sk = get_qaoa_sk_objective(
-        N, p, J=J, precomputed_energies=precomputed_energies, simulator="gpumpi", parameterization="gamma beta", objective="overlap"
-    )(gamma, beta)
-    o2_gpumpi_sk = get_qaoa_sk_objective(N, p, J=J, simulator="gpumpi", parameterization="gamma beta", objective="overlap")(gamma, beta)
-
-    print(np.all([np.isclose(o1_c_sk, np.real(o1_gpumpi_sk)), np.isclose(o1_c_sk, np.real(o2_gpumpi_sk))]))
-
+    for objective in ["overlap", "expectation"]:
+        o1_c_sk = get_qaoa_sk_objective(
+            N, p, J=J, precomputed_energies=precomputed_energies, simulator="c", parameterization="gamma beta", objective="overlap"
+        )(gamma, beta)
+        o1_gpumpi_sk = get_qaoa_sk_objective(
+            N, p, J=J, precomputed_energies=precomputed_energies, simulator="gpumpi", parameterization="gamma beta", objective="overlap"
+        )(gamma, beta)
+        o2_gpumpi_sk = get_qaoa_sk_objective(
+            N, p, J=J, simulator="gpumpi", parameterization="gamma beta", objective="overlap"
+        )(gamma, beta)
+        assert np.isclose(o1_c_sk, o1_gpumpi_sk)
+        assert np.isclose(o1_c_sk, o2_gpumpi_sk)
 
 if __name__ == "__main__":
 
-    gpumpi_test()
+    gpumpi_labs_test()
+    gpumpi_maxcut_test()
+    gpumpi_sk_test()
