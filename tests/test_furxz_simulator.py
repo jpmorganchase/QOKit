4e804d9de66cf809aadaf23bad730ed2ddec8543
Shaydulin, Ruslan <ruslan.shaydulin@jpmchase.com>
2026-02-25 17:59:50 -0500

    add reason for skipping the test

--- tests/test_furxz_simulator.py
+++ tests/test_furxz_simulator.py
@@ -12,7 +12,7 @@ import pytest
 PYTHON_ONLY = False if os.environ.get("QOKIT_PYTHON_ONLY") is None else os.environ.get("QOKIT_PYTHON_ONLY")
 
 
-@pytest.mark.skipif(PYTHON_ONLY)
+@pytest.mark.skipif(PYTHON_ONLY, reason="Fast c/c++ simulator is not installed if PYTHON_ONLY is set")
 def test_furxz_backends():
 
     N = 10
